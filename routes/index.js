91aa9b900725c264d8ce6f905e78cc414b7f4cb7
PrasanthTata <73434266+prasanth1107@users.noreply.github.com>
2021-11-17 16:45:02 -0500

    Initial folder structure

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const userRoutes = require('./user');
+const productRoutes = require('./product');
+const cartRoutes = require('./cart');
+
+const routes = (app) => {
+  app.use('/user', userRoutes);
+  app.use('/product', productRoutes);
+  app.use('/cart', cartRoutes);
+
+  app.use('*', (req, res) => {
+    res.status(404).json({ error: 'Not found' });
+  });
+};
+
+module.exports = routes;
