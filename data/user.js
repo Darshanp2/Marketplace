6336f638664c536f6643502e8fcd4130ce82964c
ahirsarthak <sarthakahir@gmail.com>
2021-12-01 20:35:26 -0500

    Login and Signup
    Created login and signup routes with authentication. As per the DB

--- data/user.js
+++ data/user.js
@@ -1,47 +1,97 @@
-const mongoCollections = require('../config/mongoCollections.js');
-const user = mongoCollections.user
-const product = mongoCollections.product
-let { ObjectId } = require('mongodb');
+const mongoCollections = require("../config/mongoCollections.js");
+const user = mongoCollections.user;
+const product = mongoCollections.product;
+let { ObjectId } = require("mongodb");
 
+const bcrypt = require("bcryptjs");
+const utils = require("./utils");
 
-async function getUser(id){
+const saltRounds = 16;
 
-    const userCollection = await user()
-    const prodCollection = await product()
+async function getUser(id) {
+  const userCollection = await user();
+  const prodCollection = await product();
 
-    let objectID = ObjectId(id)
-    const userModel = await userCollection.findOne({_id: objectID})
-    const productRes = await prodCollection.find({sellerID : id}).toArray();
-    let result = {
-        user : userModel,
-        products : productRes
+  let objectID = ObjectId(id);
+  const userModel = await userCollection.findOne({ _id: objectID });
+  const productRes = await prodCollection.find({ sellerID: id }).toArray();
+  let result = {
+    user: userModel,
+    products: productRes,
+  };
+  return result;
+}
+
+function removeObjectFromId(obj) {
+  obj["_id"] = obj["_id"].toString();
+  return obj;
+}
+
+async function updateProfile(name, email, Password, address, phone, id) {
+  let objectID = ObjectId(id);
+  const userCollection = await user();
+  console.log(name + " " + email + " " + Password + " " + address);
+  const updatedInfo = await userCollection.updateOne(
+    { _id: objectID },
+    {
+      $set: {
+        name: name,
+        address: address,
+        phoneNumber: phone,
+        email: email,
+        password: Password,
+      },
     }
-    return result
+  );
+  if (updatedInfo.modifiedCount === 0) return false;
+  return true;
 }
 
-function removeObjectFromId(obj){
-    obj["_id"] = obj["_id"].toString()
-    return obj
-}
+async function createUser(name, address, phoneNumber, email, password) {
+  //console.log("inside CreateUSer");
+  for (let i of email) {
+    if (i == " ") throw `email has empty spaces`;
+  }
+  for (let i of password) if (i == " ") throw `password has empty spaces`;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-async function updateProfile(name,email,Password,address,phone,id){
-    let objectID = ObjectId(id)
-    const userCollection = await user()
-    console.log(name+ " " + email+ " " +Password+ " " +address)
-    const updatedInfo = await userCollection.updateOne(
-        {_id: objectID},
-        { $set: { 
-            name: name,
-            address: address,
-            phoneNumber: phone,
-            email: email,
-            password: Password,
-        }}
-    )
-    if (updatedInfo.modifiedCount === 0) return false
-    return true
-}
+  if (await utils.emailExists(email)) {
+    return "email already taken";
+  }
 
+  const userCollection = await user();
+  let newUser = {
+    name: name,
+    address: address,
+    phoneNumber: phoneNumber,
+    email: email,
+    hashedPassword: hashedPassword,
+    activeCart: [],
+  };
+
+  const insertInfo = await userCollection.insertOne(newUser);
+  if (insertInfo.insertedCount === 0) throw "Failed to create a new user.";
+
+  return { userInserted: true };
+}
+async function checkUser(email, password) {
+  const userCollection = await user();
+
+  const res = await userCollection.findOne({
+    email: email,
+  });
+  if (res == null) {
+    throw `error`;
+  }
+  if (await bcrypt.compare(password, res.hashedPassword)) {
+    return { userId: res._id, authenticated: true };
+  } else {
+    throw `Password not match`;
+  }
+}
 module.exports = {
-    updateProfile,getUser
-}
+  updateProfile,
+  getUser,
+  createUser,
+  checkUser,
+};
--- routes/user.js
+++ routes/user.js
@@ -1,33 +1,180 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const data = require('../data');
+const data = require("../data");
 const userData = data.user;
 
+router.get("/updateProfile", async (req, res) => {
+  try {
+    let id = "61a7ea01a7e4d0fd6a34d230";
+    const result = await userData.getUser(id);
+    //sconsole.log(result.products)
+    res.render("posts/updateprofile", {
+      user: result.user,
+      products: result.products,
+    });
+  } catch (e) {
+    console.log(e);
+    res.json(e);
+  }
+});
+router.post("/updateProfile", async (req, res) => {
+  try {
+    let input = req.body;
+    let { Name, Email, password, Address, phone } = input;
+    let id = "61a7ea01a7e4d0fd6a34d230";
+    let updateUser = await userData.updateProfile(
+      Name,
+      Email,
+      password,
+      Address,
+      phone,
+      id
+    );
+    res.redirect("/user/updateProfile");
+  } catch (e) {
+    res.json(e);
+  }
+});
 
-router.get('/updateProfile', async (req, res) => {
-    try {
-        let id = '61a7ea01a7e4d0fd6a34d230';
-        const result = await userData.getUser(id);
-        //sconsole.log(result.products) 
-        res.render('posts/updateprofile',{
-            user : result.user,
-            products : result.products})   
-    } catch (e) {
-        console.log(e)
-        res.json(e)
+router.get("/", async (req, res) => {
+  try {
+    if (req.session.userId) {
+      res.redirect("/private");
+      return;
+    } else {
+      res.render("posts/login");
     }
-})
-router.post('/updateProfile', async (req, res) => {
-    try{
-    let input = req.body
-       let {Name,Email,password,Address,phone} = input
-       let id = '61a7ea01a7e4d0fd6a34d230';
-        let updateUser = await userData.updateProfile(Name,Email,password,Address,phone,id);
-        res.redirect('/user/updateProfile') 
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+
+router.get("/login", async (req, res) => {
+  try {
+    if (req.session.userId) {
+      res.redirect("/private");
+      return;
+    } else {
+      res.redirect("/");
     }
-    catch (e){
-        res.json(e)
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+
+router.get("/signup", async (req, res) => {
+  try {
+    if (req.session.userId) {
+      res.status(200).redirect("/private");
     }
-})
+    res.status(200).render("posts/signup", {
+      title: "Sign up",
+    });
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+
+router.post("/signup", async (req, res) => {
+  const name = req.body.name;
+  const address = req.body.address;
+  const phoneNumber = req.body.phoneNumber;
+
+  const email = req.body.email;
+  //.toString().toLowerCase().trim();
+
+  const password = req.body.password.toString();
+  //.trim().replace(/\s/g, "");
+
+  if (!email || !password) {
+    res.status(400).render("posts/signup", {
+      error: " HTTP 400 Error: Invalid input. All fields must be supplied.",
+      partial: "signup",
+    });
+    return;
+  }
+
+  try {
+    let newUser = await usersData.createUser(
+      name,
+      address,
+      phoneNumber,
+      email,
+      password
+    );
+
+    if (newUser.userInserted == true) {
+      res.redirect("/");
+    } else {
+      res.render("posts/signup", { error: "email already exists" });
+    }
+    return;
+  } catch (e) {
+    // res.json(error);
+    res.status(500).status(400).render("posts/signup", {
+      title: "Error",
+      status: "404",
+    });
+  }
+});
+
+router.get("/private", async (req, res) => {
+  const email = req.body.email;
+  const name = req.body.name;
+
+  if (!req.session.userId) {
+    res.redirect("/");
+    return;
+  }
+  res.render("posts/private", {
+    email: email,
+    name: name,
+  });
+});
+
+router.post("/login", async (req, res) => {
+  const email = req.body.email.toString().toLowerCase().trim();
+  const password = req.body.password.toString().trim();
+
+  if (!email || !password) {
+    res.status(401).render("posts/login", {
+      error: "Missing email or password.",
+    });
+    return;
+  }
+
+  // Retrieve user from file
+  try {
+    let newUser1 = await usersData.checkUser(email, password);
+    if (newUser1.authenticated == true) {
+      req.session.userId = newUser1.userId;
+      res.status(200).redirect("/private");
+      return;
+    } else {
+      res.status(401).render("posts/login", {
+        error: "Wrong email or password.",
+        email: email,
+      });
+      return;
+    }
+    // return to main page?
+  } catch (e) {
+    res.status(401).render("posts/login", {
+      hasErrors: true,
+      error: "You did not provide a valid email and/or password.",
+      title: "Login",
+      email: email,
+
+      partial: "signup",
+    });
+    return;
+  }
+});
+router.get("/logout", async (req, res) => {
+  res.clearCookie("AuthCookie");
+  res.clearCookie("Build Session");
+  req.session.destroy();
+  res.render("posts/logout");
+});
 
 module.exports = router;
