89267f804acb9afbff70df64bc8e7914513d983a
ahirsarthak <sarthakahir@gmail.com>
2021-12-05 23:36:14 -0500

    Worked on CSS
    Applied CSS on explore page, post advertisement page and on landing page.

--- app.js
+++ app.js
@@ -5,36 +5,35 @@ const static = express.static(__dirname + "/public");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
 
-
 const handlebarsInstance = exphbs.create({
-    defaultLayout: 'main',
-    // Specify helpers which are only registered on this instance.
-    helpers: {
-        asJSON: (obj, spacing) => {
-            if (typeof spacing === 'number')
-                return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
+  defaultLayout: "main",
+  // Specify helpers which are only registered on this instance.
+  helpers: {
+    asJSON: (obj, spacing) => {
+      if (typeof spacing === "number")
+        return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
 
-            return new Handlebars.SafeString(JSON.stringify(obj));
-        }
-    }
+      return new Handlebars.SafeString(JSON.stringify(obj));
+    },
+  },
 });
 
 app.use;
-app.use('/public', static);
+app.use("/public", static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.engine("handlebars", handlebarsInstance.engine);
+app.set("view engine", "handlebars");
 
-app.engine('handlebars', handlebarsInstance.engine);
-app.set('view engine', 'handlebars');
-
-
-app.use(session({
-    name: 'AuthCookie',
-    secret: 'Dont Touch Me',
+app.use(
+  session({
+    name: "AuthCookie",
+    secret: "Dont Touch Me",
     resave: false,
-    saveUninitialized: true
-}))
+    saveUninitialized: true,
+  })
+);
 
 // app.use('/private', (req, res, next) => {
 //     if (!req.session.user) {
@@ -44,23 +43,22 @@ app.use(session({
 //     }
 // });
 
-let m = function(req, res, next) {
-    let time = new Date().toUTCString();
-    let RM = req.method;
-    let RR = req.originalUrl;
-    if (!req.session.user) {
-        console.log("[" + time + "]: " + RM + " " + RR + " Non-Authenticated User");
-    } else {
+let m = function (req, res, next) {
+  let time = new Date().toUTCString();
+  let RM = req.method;
+  let RR = req.originalUrl;
+  if (!req.session.user) {
+    console.log("[" + time + "]: " + RM + " " + RR + " Non-Authenticated User");
+  } else {
+    console.log("[" + time + "]: " + RM + " " + RR + " Authenticated User");
+  }
+  next();
+};
 
-        console.log("[" + time + "]: " + RM + " " + RR + " Authenticated User");
-    }
-    next();
-}
-
-app.use(m)
+app.use(m);
 configRoutes(app);
 
 app.listen(3000, () => {
-    console.log("We've now got a server!");
-    console.log('Your routes will be running on http://localhost:3000');
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
 });
