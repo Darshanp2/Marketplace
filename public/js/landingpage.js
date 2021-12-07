5b60ccfe408e3149ed11b3718413730563adde5f
Darshan Patel <pateldarshan981@gmail.com>
2021-12-07 02:17:48 -0500

    Update profile and minor css changes.

--- public/js/landingpage.js
+++ public/js/landingpage.js
@@ -1,66 +1,15 @@
-// (function($) {
-//     // Let's start writing AJAX calls!
-
-//     var requestConfig = {
-//         method: 'GET',
-//         url: 'http://localhost:3000'
-//     };
-//     let login =$("form")
-//     let logout= $("Log-out")
-//     // let butt=$("#submit_button")
-//     // let shows=$("#show")
-//     // let search=$("#search_term")
-//     // let backlink= $("#homeLink")
-
-
-
-//     $.ajax(requestConfig).then(function() {
-//         login.show();
-//         logout.hide();
-        
-//     });
-
-//     butt.click(function(event) {
-//         event.preventDefault();
-//         shows.empty();
-//         if (search.val() === '' || !search.val().replace(/\s/g, '').length) {
-//             errortag.text("Input not valid!").show();
-//             list.empty();
-//             backlink.show();
-
-//         } else {
-
-//             var searchRequest = {
-//                 method: 'GET',
-//                 url: 'http://api.tvmaze.com/search/shows?q=' + search.val()
-//             };
-//             $.ajax(searchRequest).then(function(responseMessage) {
-//                 event.preventDefault();
-//                 list.empty();
-//                 if (responseMessage.length == 0) {
-//                     errortag.text("No results found").show();
-//                 } else {
-//                     for (let i = 0; i < responseMessage.length; i++) {
-//                         list.append(`<li><a href="${responseMessage[i].show._links.self.href}">${responseMessage[i].show.name}</a></li>`);
-//                     }
-//                     errortag.hide();
-//                 }
-//             });
-//             list.show();
-
-//             backlink.show();
-//         }
-//     });
-
-
-//     $(document).on("click", "#showList > li > a", function(event) {
-//         event.preventDefault();
-//         var href = $(this).attr('href')
-//         list.hide();
-//         shows.empty();
-//         errortag.hide();
-        
-
-//     });
-
-// })(window.jQuery);
+(function ($) {
+    $(document).on("click", "#dropbtn", function (event) {
+        event.preventDefault()
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+              var openDropdown = dropdowns[i];
+              if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+              }
+            }
+          };
+    });
+})(window.jQuery);
