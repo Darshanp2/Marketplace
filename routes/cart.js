23a6aeb0eaac4a313ad94494199cec52e2522168
PrasanthTata <73434266+prasanth1107@users.noreply.github.com>
2021-12-06 22:10:03 -0500

    Add to cart, remove from cart view cart and cart page

--- routes/cart.js
+++ routes/cart.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const { cart } = require('../data');
 const router = express.Router();
 const data = require('../data');
 const cartData = data.cart;
 
 router.get("/", async (req,res) => {
-    
+    let id = req.session.user
+    const cartResult = await cartData.getUserCart(id)
+    res.render('posts/cartPage',{
+        products : cartResult.products,
+        totalPrice : cartResult.totalPrice,
+        title : 'Cart Page'
+    })
 })
 
-router.get("/removeProduct/:id"), async (req,res) =>{
+router.get("/addToCart/:id", async (req,res) =>{
+    let userId = req.session.user
+    let prodId = req.params.id
+    const addedToCart = await cartData.addToCart(userId,prodId)
+    res.redirect("/explore")
+})
 
-}
+router.get("/removeProduct/:id", async (req,res) =>{
+    let userId = req.session.user
+    let prodId = req.params.id
+    const removeProduct = await cartData.removeFromCart(userId,prodId)
+    res.redirect("/cart/")
+})
 
 router.post("/purchase",async (req,res) =>{
 
