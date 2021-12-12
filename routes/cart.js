f58d9fdb95bba9a16b57e0f3e9ef1bb0890de759
PrasanthTata <73434266+prasanth1107@users.noreply.github.com>
2021-12-12 14:40:24 -0500

    placeOrder fixes, orderHistory, orderDetails pages

--- routes/cart.js
+++ routes/cart.js
@@ -5,27 +5,49 @@ const data = require('../data');
 const cartData = data.cart;
 
 router.get("/", async (req,res) => {
+    try{
     let id = req.session.user
     const cartResult = await cartData.getUserCart(id)
     res.render('posts/cartPage',{
         products : cartResult.products,
         totalPrice : cartResult.totalPrice,
         title : 'Cart Page'
-    })
+    })}
+    catch(e){
+        res.render('posts/cartPage',{ error : e})
+}
 })
 
 router.get("/addToCart/:id", async (req,res) =>{
-    let userId = req.session.user
+    try{let userId = req.session.user
     let prodId = req.params.id
+    validateId(prodId)
     const addedToCart = await cartData.addToCart(userId,prodId)
-    res.redirect("/product/exploreproduct")
+    res.redirect("/product/exploreproduct")}
+    catch(e){
+        if(e[0] == 405){
+            res.status(e[0]).render('posts/error',{error : e[1] , status: e[0]})
+        }
+        else{
+            res.render('posts/cartPage',{ error : e})
+        }
+    }
 })
 
 router.get("/removeProduct/:id", async (req,res) =>{
-    let userId = req.session.user
+   try {let userId = req.session.user
     let prodId = req.params.id
+    validateId(prodId)
     const removeProduct = await cartData.removeFromCart(userId,prodId)
-    res.redirect("/cart/")
+    res.redirect("/cart/")}
+    catch(e){
+        if(e[0] == 405){
+            res.status(e[0]).render('posts/error',{error : e[1] , status: e[0]})
+        }
+        else{
+            res.render('posts/cartPage',{ error : e})
+        }
+    }
 })
 
 router.post("/purchase",async (req,res) =>{
@@ -36,10 +58,28 @@ router.post("/purchase",async (req,res) =>{
 
 router.get("/orderHistory", async (req,res) =>{
     let userId = req.session.user
-    let orders = await fetchOrders(userId)
+    let orders = await cartData.fetchOrders(userId)
     res.render('posts/orderHistory',{orders:orders})
 })
 
+router.get("/orderDetails/:id", async (req,res) =>{
+    try{let id = req.params.id
+    validateId(prodId)
+    const cartResult = await cartData.getOrder(id)
+    res.render('posts/orderDetails',{
+        products : cartResult.products,
+        totalPrice : cartResult.totalPrice,
+        title : 'Order Details Page'
+    })}
+    catch(e){
+        res.status(e[0]).render('posts/error',{error : e[1] , status: e[0]})
+    }
+})
 
+function validateId(id){
+    if(typeof id !== "string") throw [405,"invalid ID"]
+    if (!id || id.trim().length ==0) throw [405,"invalid ID"]
+    if(!/^[0-9A-Fa-f]{24}$/.test(id)) throw [405,"invalid ID"]
+  }
 
 module.exports = router;
